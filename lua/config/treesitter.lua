d0c8868650115bf208fd51ea3103b155076f195a
moexco <34889451+moexco@users.noreply.github.com>
2025-11-22 23:52:20 +0800

    fix: 修复语法高亮失效的问题

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,13 +1,13 @@
 -- lua/config/treesitter.lua
 -- Treesitter 插件配置
 
-require("nvim-treesitter.config").setup({
-	ensure_installed = { "go", "lua", "html", "rust" },
-	auto_install = true,
-	highlight = {
-		enable = true,
-	},
-	indent = { enable = true },
-	incremental_selection = { enable = true }, -- 增量选择
-	context_commentstring = { enable = true }, -- 注释字符串
+require("nvim-treesitter").setup({})
+
+
+require("nvim-treesitter").install({ 'rust', 'go', 'lua' })
+
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'rust', 'go', 'lua' },
+  callback = function() vim.treesitter.start() end,
 })
