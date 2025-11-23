c39b53d1dcdb0aa57f85a0c861d706d0e4d344d5
moexco <34889451+moexco@users.noreply.github.com>
2025-11-23 16:21:25 +0800

    style: nvim-tree的git状态使用颜色而不是icon表示

--- lua/config/nvimtree.lua
+++ lua/config/nvimtree.lua
@@ -6,7 +6,24 @@ require("nvim-tree").setup({
 		enable = true,
 		ignore = false, -- Set to true to ignore files in .gitignore
 	},
+	renderer = {
+		highlight_git = true, -- 启用 Git 状态高亮
+		icons = {
+			show = {
+				git = false, -- 禁用 Git 状态图标
+			},
+		},
+	},
 })
+
+-- 定义 Git 状态的高亮组
+-- 您可以根据自己的喜好调整颜色
+vim.cmd.highlight("NvimTreeGitStaged guifg=#aff5b4")   -- 已暂存 (绿色)
+vim.cmd.highlight("NvimTreeGitDirty guifg=#e3b341")    -- 已修改 (黄色)
+vim.cmd.highlight("NvimTreeGitNew guifg=#56d364")      -- 新增 (绿色)
+vim.cmd.highlight("NvimTreeGitRenamed guifg=#f0883e")   -- 已重命名 (橙色)
+vim.cmd.highlight("NvimTreeGitDeleted guifg=#f85149")  -- 已删除 (红色)
+vim.cmd.highlight("NvimTreeGitIgnored guifg=#909090")  -- 已忽略 (灰色)
 
 -- 切换文件树
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "切换文件树" })
